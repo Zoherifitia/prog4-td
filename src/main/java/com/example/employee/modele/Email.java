b885cb68a8fbc35fbb65b1e5c1e5399c1d61ecd8
Zoherifitia <hei.nitsiaro@gmail.com>
2023-07-22 16:07:55 +0300

    chore: example modele

--- src/main/java/com/example/employee/modele/Email.java
+++ src/main/java/com/example/employee/modele/Email.java
@@ -1,2 +1,18 @@
-package com.example.employee.modele;public class Email {
+package com.example.employee.modele;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Email {
+    @Id
+    private Integer id;
+    private String emailPro;
+    private String emailPerso;
 }
--- src/main/java/com/example/employee/modele/Phone.java
+++ src/main/java/com/example/employee/modele/Phone.java
@@ -1,2 +1,17 @@
-package com.example.employee.modele;public class Phone {
+package com.example.employee.modele;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Phone {
+    @Id
+    private Integer id;
+    private String number;
 }
